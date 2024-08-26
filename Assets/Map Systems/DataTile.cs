4763ea6f1ffb50a8d3c0f6790d68568216758757
Zero497 <crscott1@arizona.edu>
2024-08-26 15:58:32 -0700

    initial commit

---
+++ Assets/Map Systems/DataTile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+//Purpose: extend the tile class to store a reference to data about the tile.
+public class DataTile : Tile
+{
+    public TileData data;
+}
---
+++ Assets/Map Systems/TileData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Purpose: store the properties of a specific tile
+public class TileData : ScriptableObject
+{
+    //how many tiles of movement this tile costs to enter
+    public float moveCost;
+}
